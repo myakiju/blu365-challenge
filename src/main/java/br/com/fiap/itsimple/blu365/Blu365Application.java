0fbacdc8b706d8968ede0f3740aba9c74cf3926d
Logon Aluno <logonrmlocal@fiap.com.br>
2018-11-24 14:04:28 -0200

    first commit

---
+++ src/main/java/br/com/fiap/itsimple/blu365/Blu365Application.java
@@ -0,0 +1,12 @@
+package br.com.fiap.itsimple.blu365;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blu365Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blu365Application.class, args);
+	}
+}
---
+++ src/main/java/br/com/fiap/itsimple/blu365/api/client/ReceitaFederalClient.java
@@ -0,0 +1,5 @@
+package br.com.fiap.itsimple.blu365.api.client;
+
+public class ReceitaFederalClient {
+	  
+}
